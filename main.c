90f6809319294e7ce445912d43c9ead02ae65d63
gcoayla <gcoayla.28@gmail.com>
2025-01-27 11:39:06 -0500

    carga de archivo minerva

--- main.c
+++ main.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
-#define NOMBRE_FICHERO "gasolina.csv"
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 1024
+#define MAX_RECORDS 1000
 
 typedef struct {
     int id;
-    char provincia[20];
-    char localidad[20];
-    char postal[4];
-    char direccion[60];
-    char margen;
-    char rotulo[60];
+    char provincia[50];
+    char localidad[50];
+    char codigo_postal[10];
+    char direccion[100];
+    char margen[2];
+    char rotulo[50];
     float gasolina95;
     float gasolina98;
     float gasoleoA;
-} RegistroGasolina;
+} Gasolinera;
 
 // ----------------- Funciones de utilidad
+
+int cargar_datos(const char *filename, Gasolinera gasolineras[]) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("No se pudo abrir el archivo: %s. Verifique que existe y tiene permisos de lectura.\n", filename);
+        return -1;
+    }
+
+    char buffer[MAX_LINE];
+    int count = 0;
+
+    while (fgets(buffer, MAX_LINE, file) && count < MAX_RECORDS) {
+        if (count == 0) {
+            count++;
+            continue;
+        }
+
+        sscanf(buffer, "%d,%49[^,],%49[^,],%9[^,],%99[^,],%1[^,],%49[^,],%f,%f,%f",
+               &gasolineras[count - 1].id,
+               gasolineras[count - 1].provincia,
+               gasolineras[count - 1].localidad,
+               gasolineras[count - 1].codigo_postal,
+               gasolineras[count - 1].direccion,
+               gasolineras[count - 1].margen,
+               gasolineras[count - 1].rotulo,
+               &gasolineras[count - 1].gasolina95,
+               &gasolineras[count - 1].gasolina98,
+               &gasolineras[count - 1].gasoleoA);
+
+        count++;
+    }
+
+    fclose(file);
+    return count - 1;
+}
 
 // Solicita un numero al usuario y se asegura que el valor introducido sea un numero entero
 int obtenerEntero() {
@@ -58,18 +97,16 @@ void imprimirRegistrosOrdenados() {
 }
 
 int main(void) {
-    FILE *fichero;
-    fichero = fopen(NOMBRE_FICHERO, "r");
+    Gasolinera gasolineras[MAX_RECORDS];
+    char ruta[256];
+    printf("Ingrese la ruta del archivo CSV: ");
+    scanf("%255s", ruta);
 
-    RegistroGasolina registros[180];
-
-    if (fichero == NULL) {
-        printf("Error al abrir el fichero");
-        return -1;
-    } else {
-
+    int total_gasolineras = cargar_datos(ruta, gasolineras);
+    if (total_gasolineras < 0) {
+        return 1;
     }
 
-    fclose(fichero);
+    printf("Archivo cargado correctamente. Total de registros: %d\\n", total_gasolineras);
     return 0;
 }
